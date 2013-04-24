2a06c426b4f3df61f9e76d7417c33d228720e374
John Emmas <johne53@tiscali.co.uk>
2013-04-24 09:11:21 +0100

    JOHNE - Add my files (except for the VS project and solution)

---
+++ win32-fixup.pl
@@ -0,0 +1,30 @@
+#! e:/program files/perl/bin/perl.exe
+#  proxy-libintl has no version info (presumably 'intl.dll' has it)
+
+require "local-paths.lib";
+
+sub process_file
+{
+        my $outfilename = shift;
+	my $infilename = $outfilename . ".in";
+	
+	open (INPUT, "< $infilename") || exit 1;
+	open (OUTPUT, "> $outfilename") || exit 1;
+	
+	while (<INPUT>) {
+	    s/\@GenericIncludeFolder@/$generic_include_folder/g;
+	    s/\@GenericLibraryFolder@/$generic_library_folder/g;
+	    s/\@GenericWin32LibraryFolder@/$generic_win32_library_folder/g;
+	    s/\@Debug32TestSuiteFolder@/$debug32_testsuite_folder/g;
+	    s/\@Release32TestSuiteFolder@/$release32_testsuite_folder/g;
+	    s/\@Debug32TargetFolder@/$debug32_target_folder/g;
+	    s/\@Release32TargetFolder@/$release32_target_folder/g;
+	    s/\@TargetSxSFolder@/$target_sxs_folder/g;
+	    print OUTPUT;
+	}
+}
+
+my $command=join(' ',@ARGV);
+if ($command eq -buildall) {
+	process_file ("msvc/intl.vsprops");
+}
